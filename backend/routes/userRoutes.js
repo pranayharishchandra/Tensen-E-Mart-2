1efa0f2608626eef1cb0f7742feed24c2b638646
pranay <pranayharishchandra@gmail.com>
2024-03-16 10:43:52 +0530

    userRouter

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,19 +12,21 @@ import {
 } from '../controllers/userController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
-const router = express.Router();
+const userRouter = express.Router();
 
-router.route('/').post(registerUser).get(protect, admin, getUsers);
-router.post('/auth', authUser);
-router.post('/logout', logoutUser);
-router
+userRouter.route('/').post(registerUser).get(protect, admin, getUsers);
+userRouter.post('/auth', authUser);
+userRouter.post('/logout', logoutUser);
+
+userRouter
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
-router
+  
+userRouter
   .route('/:id')
   .delete(protect, admin, deleteUser)
   .get(protect, admin, getUserById)
   .put(protect, admin, updateUser);
 
-export default router;
+export default userRouter;
